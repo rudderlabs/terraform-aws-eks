76e267668f8a76a565e9a346ca4dbf7480fdb46a
Subbarao Vakati <subbu@rudderlabs.com>
2020-01-21 16:10:21 +0530

    EKS and vpc creation

---
+++ variables.tf
@@ -0,0 +1,105 @@
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+
+  default = [
+    "422074288268",
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      rolearn  = "arn:aws:iam::422074288268:role/eks-access"
+      username = "rudder"
+      groups   = ["cluster-admin"]
+    },
+  ]
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These variables are expected to be passed in by the operator.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "region" {
+  description = "The region used for the vpc network and the eks cluster."
+  type        = string
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "cluster_name" {
+  description = "The name of the Kubernetes cluster."
+  type        = string
+  default     = "rudder-cluster"
+}
+
+variable "cluster_service_account_name" {
+  description = "The name of the custom service account used for the GKE cluster. This parameter is limited to a maximum of 28 characters."
+  type        = string
+  default     = "rudder-cluster-sa"
+}
+
+variable "cluster_service_account_description" {
+  description = "A description of the custom service account used for the GKE cluster."
+  type        = string
+  default     = "Rudder GKE Cluster Service Account managed by Terraform"
+}
+
+# For the example, we recommend a /16 network for the VPC. Note that when changing the size of the network,
+# you will have to adjust the 'cidr_subnetwork_width_delta' in the 'vpc_network' -module accordingly.
+variable "vpc_cidr_block" {
+  description = "The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
+  type        = string
+  default     = "10.6.0.0/16"
+}
+
+variable "vpc_cidr_subnetwork_width_delta" {
+  description = "The difference between your network and subnetwork netmask; an /16 network and a /20 subnetwork would be 4."
+  type        = number
+  default     = 4
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# TEST PARAMETERS
+# These parameters are only used during testing and should not be touched.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "override_default_node_pool_service_account" {
+  description = "When true, this will use the service account that is created for use with the default node pool that comes with all GKE clusters"
+  type        = bool
+  default     = true
+}
+
+variable "rudder_node_type" {
+  description = "Google compute engine instance type for worker nodes"
+  type        = string
+  default     = "m5.large"
+}
+
+variable "rudder_disk_size_gb" {
+  description = "Default disk size on each worker node. Used for logs and temporary storage."
+  type        = string
+  default     = "30"
+}
